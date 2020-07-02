d01e41c16ba0d6030e7c375d254a636600a56198
Diogo Casagrande <diogo.casagrande@mzgroup.com>
2020-07-02 19:02:08 -0300

    feat(bash.sh): add bash.sh para enpacotar o zip

---
+++ bash.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+if [ -e "lambda.zip" ]; then
+   rm lambda.zip
+fi
+
+virtualenv env
+source env/bin/activate
+
+pip3 install -r requirements.txt
+
+mkdir dist
+cp -rf env/lib/python3.*/site-packages/* dist
+
+cp src/*.py dist
+
+cd dist
+zip -r lambda.zip
+
+cd ..
+mv dist/lambda.zip .
+rm -rf dist env
